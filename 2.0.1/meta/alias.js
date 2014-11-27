275a64ea3d5bedc918c26694a917663c241faa08
弘树 <tiehang.lth@alibaba-inc.com>
2014-11-27 14:00:51 +0800

    sync daily version 2.0.1

---
+++ 2.0.1/meta/alias.js
@@ -0,0 +1,3 @@
+config({
+    'kg/newbie-guide/index': {alias: ['kg/newbie-guide/2.0.1/index']}
+});
---
+++ 2.0.1/mini.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview 
+ * @author 圆影<yuanying.xh@alibaba-inc.com>
+ * @module newbie-guide
+ **/
+KISSY.add(function(S, Component) {
+
+  return Component;
+
+}, {
+  requires: ["./index"]
+});
