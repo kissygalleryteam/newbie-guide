6653a2182b4f860493e7c1fef6f7273679843447
kissyteam <tiehang.lth@taobao.com>
2015-01-28 21:37:07 +0800

    sync daily version 0.0.1

--- 0.0.1/index.js
+++ 0.0.1/index.js
@@ -1,21 +1,8 @@
 var $ = require('node').all;
-var Base = require('base');
 
-var Guide = Base.extend({
-    initializer:function(){
-        var self = this;
-        var $target = self.get('$target');
-    }
-},{
-    ATTRS:{
-        $target:{
-            value:'',
-            getter:function(v){
-                return $(v);
-            }
-        }
-    }
-});
+var Guide = function() {
+
+};
 
 module.exports = Guide;
 
